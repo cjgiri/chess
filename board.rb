cac4db81dd53285afadcbbfdbc6e9f4ab1c377b3
Abhijith Settipalli <baittes@gmail.com>
2016-04-18 17:04:48 -0400

    Added move searching for all pieces (except pawn)

--- board.rb
+++ board.rb
@@ -7,39 +7,69 @@ class Board
 
   def initialize
     @grid= Array.new(8) {Array.new(8)}
-    @grid.first(2).each do |row|
-      row.length.times do |index|
-        row[index] = Piece.new(:w)
-      end
-    end
+    # @grid.first(2).each do |row|
+    #   row.length.times do |index|
+    #     row[index] = Piece.new(:w,self)
+    #   end
+    # end
+    #
+    # @grid.last(2).each do |row|
+    #   row.length.times do |index|
+    #     row[index] = Piece.new(:b,self)
+    #   end
+    # end
+  end
 
-    @grid.last(2).each do |row|
-      row.length.times do |index|
-        row[index] = Piece.new(:b)
-      end
-    end
+  # def inspect
+  #
+  # end
 
-
+  def piece(row,col)
+    grid[row][col].color
   end
 
   def move(start_pos,end_pos)
     if grid[*start_pos].nil? || !Board.in_bounds?(end_pos)
       raise ArgumentError
     end
-    grid[*end_pos] = grid[*start_pos]
-    grid[*start_pos] = nil
+    grid[end_pos[0]][end_pos[1]] = grid[start_pos[0]][start_pos[1]]
+    grid[start_pos[0]][start_pos[1]] = nil
   end
 
   def self.in_bounds?(pos)
     (0..7).include?(pos[0]) && (0..7).include?(pos[1])
   end
 
+  def add_new_piece(color,pos,type)
+    case type
+    when :R
+      p = Rook.new(color,self,pos)
+    when :Q
+      p = Queen.new(color,self,pos)
+    when :N
+      p = Knight.new(color,self,pos)
+    when :K
+      p = King.new(color,self,pos)
+    when :B
+      p = Bishop.new(color,self,pos)
+    else
+      p = Pawn.new(color,self,pos)
+    end
+    grid[pos[0]][pos[1]] = p
+  end
+
 end
+
 if __FILE__ == $PROGRAM_NAME
   b=Board.new
   d=Display.new(b)
-  until false == true
-    d.render
-    d.get_input
-  end
+  b.add_new_piece(:b,[3,3],:R)
+  p b.grid[3][3].moves
+  puts
+  b.add_new_piece(:w,[2,2],:Q)
+  p b.grid[2][2].moves
+  # until false == true
+  #   d.render
+  #   d.get_input
+  # end
 end
--- piece.rb
+++ piece.rb
@@ -1,9 +1,13 @@
+require 'byebug'
+
 class Piece
   attr_reader :color, :pos
 
-  def initialize(color, pos=[0,0])
+  def initialize(color, board, pos=[0,0])
     @color = color
     @pos = pos
+    @board = board
+    # @possible_moves = []
   end
 
   def to_s
@@ -11,7 +15,98 @@ class Piece
     return " B "
   end
 
+  def moves
+    @possible_moves.dup
+  end
+
+  private
+  attr_reader :board
+
 end
+
+class SlidingPiece < Piece
+  def moves
+    possible_moves = []
+    move_dirs.each do |dir|
+      (1..7).each do |mult|
+        poss_move_x = mult*dir[0]+pos[0]
+        poss_move_y = mult*dir[1]+pos[1]
+        poss_move = [poss_move_x,poss_move_y]
+        break unless Board.in_bounds?(poss_move)
+        # byebug if board.piece(*poss_move) != color
+        if board.grid[poss_move_x][poss_move_y].nil?
+          possible_moves << poss_move
+        elsif board.piece(*poss_move) != color
+          possible_moves << poss_move
+          break
+        else
+          break
+        end
+      end
+    end
+
+    return possible_moves
+  end
+end
+
+class SteppingPiece < Piece
+  def moves
+    possible_moves = move_dirs.map do |offset|
+      [ pos[0] + offset[0], pos[1] + offset[1] ]
+    end.select{ |poss_move| Board.in_bounds?(poss_move) }
+
+    possible_moves.select do |poss_move|
+      board.grid[poss_move[0]][poss_move[1]].nil? ||
+      board.piece(*poss_move) != color
+    end
+  end
+end
+
+class Bishop < SlidingPiece
+  def move_dirs
+    [[-1, -1],     [-1, 1],
+            #center
+    [1, -1],        [1, 1]]
+  end
+end
+
+class Rook < SlidingPiece
+  def move_dirs
+           [[-1, 0],
+    [0, -1],        [0, 1],
+            [1, 0]]
+  end
+end
+
+class Queen < SlidingPiece
+  def move_dirs
+    [[-1, -1], [-1, 0], [-1, 1],
+     [ 0, -1],          [0, 1],
+     [ 1, -1], [ 1, 0], [1, 1 ]]
+  end
+end
+
+class Knight < SteppingPiece
+  def move_dirs
+    [      [-2, -1], [-2, 1],
+    [-1, -2],              [-1, 2],
+    [ 1, -2],              [ 1, 2],
+           [2, -1],   [ 2, 1]       ]
+  end
+end
+
+class King < SteppingPiece
+  def move_dirs
+    [[-1, -1], [-1, 0], [-1, 1],
+     [ 0, -1],          [0, 1],
+     [ 1, -1], [ 1, 0], [1, 1 ]]
+  end
+end
+
+class Pawn < Piece
+
+end
+
 
 class NilClass
 
